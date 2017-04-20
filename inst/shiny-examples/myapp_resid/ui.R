c1e57fdd8e0a5f3f48351f3c298dadd217dbcd64
proto4426 <antoine.pissoort@student.uclouvain.be>
2017-04-20 22:10:28 +0200

    Finish Shiny#2

---
+++ inst/shiny-examples/myapp_resid/ui.R
@@ -0,0 +1,43 @@
+library(shiny)
+
+data("data.mat2.fin")
+
+
+names_stations <- colnames(data.mat2.fin)
+names_stations <- gsub("-", "", names_stations)
+
+
+shinyUI(fluidPage(
+  
+  titlePanel("Val-U-Sun project! Compare 2 methods by the residuals"),
+  
+  # Sidebar with a slider input for the number of bins
+  fluidRow(
+    column(3,
+           selectInput("stations",
+                       "Which Station ? (1st row)",
+                       names_stations),
+           selectInput("stations2",
+                       "Which second Station ? (2nd row)",
+                       names_stations)
+    ),
+    width = "100px",
+    #)
+    # checkboxInput("na", "Show only filled values?"),
+    column(3, offset = 1,
+           selectInput("na", "Compare interpolsvd with ?",
+                       c("Filled from Chris" = "chris",
+                         "Silso: same serie everywhere (green)" = "silso",
+                          "Filled with splines" = "splines")),
+           
+           checkboxInput("points", "Points ?")
+           # numericInput("size", "Size of points", value = 0.2, min = 0, max = 1)
+    ),
+    
+    # Show a plot of the generated distribution
+    #mainPanel(
+    plotOutput("plot1", height = '650px', width = "900px")
+    #)
+    
+  )
+))
